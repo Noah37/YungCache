2209d4040af488ce88040c3a5982864885803a33
caonongyun <caonongyun@guazi.com>
2020-12-17 21:17:45 +0800

    modify NSString

--- YungCache/Classes/Cache+Yung.swift
+++ YungCache/Classes/Cache+Yung.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 public extension Cache {
-    func getObj<T:NSCoding>(forKey key: NSString) -> T? {
-        guard let data = object(forKey: key) as? Data else { return nil }
+    func getObj<T:NSCoding>(forKey key: String) -> T? {
+        guard let data = object(forKey: key.OCString) as? Data else { return nil }
         let obj = NSKeyedUnarchiver.unarchiveObject(with: data)
         return obj as? T
     }
 
-    func setObj<T:NSCoding>(_ obj: T, forKey key: NSString) {
+    func setObj<T:NSCoding>(_ obj: T, forKey key: String) {
         let obj = NSKeyedArchiver.archivedData(withRootObject: obj)
-        setObject(obj as! ObjectType, forKey: key)
+        setObject(obj as! ObjectType, forKey: key.OCString)
     }
     
-    func setObjs<T:NSCoding>(_ objs: [T], forKey key: NSString) {
+    func setObjs<T:NSCoding>(_ objs: [T], forKey key: String) {
         let obj = NSKeyedArchiver.archivedData(withRootObject: objs)
-        setObject(obj as! ObjectType, forKey: key)
+        setObject(obj as! ObjectType, forKey: key.OCString)
     }
 }
